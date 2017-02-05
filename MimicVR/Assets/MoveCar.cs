e247dd438b85e9092e0e9cce631487dae16d7b86
RubenTipparach <santipab.stephane@gmail.com>
2017-02-05 14:40:44 -0600

    Initial Check in for base code.

---
+++ MimicVR/Assets/MoveCar.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCar : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Vector3 v = new Vector3(0, 0, 1);
+		transform.Translate(Vector3.forward * Time.deltaTime, Space.Self);
+	}
+}
