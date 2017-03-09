ce95495f71e2261c14973d514fcfa6e28d993117
DAN-BUILD-1\danie <daniel.ogburn13@gmail.com>
2017-03-08 23:10:21 -0600

    Should add "Dan" folder to the scripts folder and have my script in it.

---
+++ MimicVR/Assets/Scripts/Dan/LinearMove.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinearMove : MonoBehaviour {
+
+    [SerializeField]
+    SocketMoveCmd socket;
+
+    [SerializeField]
+    LocalMoveCmd local;
+
+    RobotCommandInput moveCmd;
+
+    Vector3 currentPosition;
+    Vector3 lastPosition;
+    float deltaPosition;
+    float velocity;
+    float averageVelocity = 0;
+
+    void Start () {
+        if (socket != null)
+        {
+            moveCmd = socket;
+        }
+        else
+        {
+            moveCmd = local;
+        }
+
+        StartCoroutine(startDelay(0));
+    }
+
+    IEnumerator startDelay(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+
+        float addedSeconds = 1;
+        int i = 0;
+
+        while (i < 5) {
+
+            lastPosition = transform.position;
+
+            moveCmd.Forward();
+
+            currentPosition = transform.position;
+            deltaPosition = currentPosition.magnitude - lastPosition.magnitude;
+            velocity = deltaPosition / 1;
+
+            StartCoroutine(moveAction(moveCmd.Backward, 2 * addedSeconds));
+
+            lastPosition = currentPosition;
+            currentPosition = transform.position;
+            deltaPosition = currentPosition.magnitude - lastPosition.magnitude;
+            velocity = deltaPosition / 1;
+
+            averageVelocity = (averageVelocity + velocity) / 2;
+            i++;
+        }
+
+        StartCoroutine(moveAction(moveCmd.Stop, 5 * addedSeconds));
+
+        Debug.Log("Velocity: " + averageVelocity);
+    }
+
+    IEnumerator moveAction(Action run, float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        run();
+    }
+	
+    void Update () {
+		
+    }
+}
