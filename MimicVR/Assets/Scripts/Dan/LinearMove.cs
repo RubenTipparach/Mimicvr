1b90a4329ca191f92ff84ba594001ae20be358a9
DAN-BUILD-1\danie <daniel.ogburn13@gmail.com>
2017-03-26 19:44:41 -0500

    Further progress on the linear movement tracking.

--- MimicVR/Assets/Scripts/Dan/LinearMove.cs
+++ MimicVR/Assets/Scripts/Dan/LinearMove.cs
@@ -15,9 +15,15 @@ public class LinearMove : MonoBehaviour {
 
     Vector3 currentPosition;
     Vector3 lastPosition;
+    Vector3 startPosition;
+    Quaternion startRotation;
+    float degreesFromStart;
+    float distanceToStart;
     float deltaPosition;
-    float velocity;
+    float velocity1;
+    float velocity2;
     float averageVelocity = 0;
+    
 
     void Start () {
         if (socket != null)
@@ -29,40 +35,69 @@ public class LinearMove : MonoBehaviour {
             moveCmd = local;
         }
 
-        StartCoroutine(startDelay(0));
+        StartCoroutine(
+            startDelay(5));
+        //startDelay(0);
     }
 
     IEnumerator startDelay(float waitTime)
     {
-        yield return new WaitForSeconds(waitTime);
 
         float addedSeconds = 1;
         int i = 0;
 
-        while (i < 5) {
+        startPosition = transform.position;
+        startRotation = transform.rotation;
 
+        while (i < 3)
+        {
             lastPosition = transform.position;
 
-            moveCmd.Forward();
+            StartCoroutine(moveAction(moveCmd.Forward, 1 * addedSeconds));
+            StartCoroutine(moveAction(moveCmd.Stop, 3 * addedSeconds));
+
+            yield return new WaitForSeconds(waitTime * addedSeconds);
 
             currentPosition = transform.position;
-            deltaPosition = currentPosition.magnitude - lastPosition.magnitude;
-            velocity = deltaPosition / 1;
+            deltaPosition = Vector3.Distance(transform.position, lastPosition);
+            velocity1 = deltaPosition / 2;
 
-            StartCoroutine(moveAction(moveCmd.Backward, 2 * addedSeconds));
+            StartCoroutine(moveAction(moveCmd.Backward, 1 * addedSeconds));
+            StartCoroutine(moveAction(moveCmd.Stop, 3 * addedSeconds));
+
+            yield return new WaitForSeconds(waitTime * addedSeconds);
 
             lastPosition = currentPosition;
             currentPosition = transform.position;
-            deltaPosition = currentPosition.magnitude - lastPosition.magnitude;
-            velocity = deltaPosition / 1;
+            deltaPosition = Vector3.Distance(transform.position, lastPosition);
+            velocity2 = deltaPosition / 2;
 
-            averageVelocity = (averageVelocity + velocity) / 2;
+            averageVelocity = (averageVelocity + (velocity1 + velocity2) / 2) / 2;
+
             i++;
         }
 
-        StartCoroutine(moveAction(moveCmd.Stop, 5 * addedSeconds));
+        distanceToStart = Vector3.Distance(transform.position, startPosition);
+        degreesFromStart = Quaternion.Angle(transform.rotation, startRotation);
 
+        while (distanceToStart > 0.5)
+        {
+            yield return new WaitForSeconds(waitTime * addedSeconds);
+            distanceToStart = Vector3.Distance(transform.position, startPosition);
+            yield return new WaitForSeconds(waitTime * addedSeconds);
+            StartCoroutine(moveAction(moveCmd.Forward, 0));
+            StartCoroutine(moveAction(moveCmd.Stop, (distanceToStart / averageVelocity) * 2));
+            Debug.Log("Distance to Start: " + distanceToStart);
+        }
+        
         Debug.Log("Velocity: " + averageVelocity);
+        
+        Debug.Log("Angle from Start: " + degreesFromStart);
+        Debug.Log("Distance to point: " + Vector3.Distance(Vector3.one, transform.position));
+
+        
+
+
     }
 
     IEnumerator moveAction(Action run, float waitTime)
