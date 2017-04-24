f12b56373ecf284b94ddcafc6c268352e0d63489
RubenTipparach <santipab.stephane@gmail.com>
2017-04-24 00:04:27 -0500

    All data processed.

--- server/data-parser/data-parser.js
+++ server/data-parser/data-parser.js
@@ -1,52 +1,78 @@
 //var logdata = require('./all-logs-slim.json');
-
 var fs = require('fs');
+var json2csv = require('json2csv');
 
-fs.readFile('./all-logs-slim.json', 'utf8', function (err,data) {
-  if (err) {
-    return console.log(err);
-  }
-  var arr = data.split('\r\n');
-  
-  var vrbots = [];
-  var simbots = [];
-  
-  for(var a of arr)
-  {
-	  var item = (JSON.parse(a));
-	if(item.robot_sim ){
+const testFolder = './test_data/';
 
-		simbots.push(item);
-	}
-	else if(item.robot_vr)
-	{
-		
-		vrbots.push(item);
-	}
-  }
-  
-  console.log(vrbots);
-  
-	var json2csv = require('json2csv');
-	var fields = ['position.x', 'position.y', 'position.z'];
-	 
-	 
-	try {
-	  var result = json2csv({ data: vrbots[0].robot_vr, fields: fields });
-	  console.log(result);
-	  
-	  fs.writeFile("./result.csv", result, function(err) {
-		if(err) {
-			return console.log(err);
-		}
+fs.readdir(testFolder, (err, files) => {
+    files.forEach((file) => {
+        console.log(file);
 
-		console.log("The file was saved!");
-		}); 
-		
-	} catch (err) {
-	  // Errors are thrown for bad options, or if the data is empty and no fields are provided. 
-	  // Be sure to provide fields if it is possible that your data array will be empty. 
-	  console.error(err);
-	}
+        fs.readFile(testFolder + file, 'utf8', function (err, data) {
+            if (err) { return console.log(err); }
+
+            var arr = data.split('\r\n');
+            var vrbots = [];
+            var simbots = [];
+
+            // push data in an array of vr runs or simbots runs
+            for (let i = 0; i < arr.length -1; i ++) {
+                a = arr[i];
+                var item = (JSON.parse(a));
+                if (item.robot_sim) {
+
+                    simbots.push(item);
+                }
+                else if (item.robot_vr) {
+
+                    vrbots.push(item);
+                }
+            }
+
+            //console.log(vrbots);
+
+     
+            var fields = ['robot_vr.position.x', 'robot_vr.position.y', 'robot_vr.position.z'];
+
+    
+            transformRawData(vrbots, fields, 'robot_vr' + file);
+
+            var fields = ['robot_sim.position.x', 'robot_sim.position.y', 'robot_sim.position.z'];
+
+            transformRawData(simbots, fields, 'robot_sim' + file);
+    
+
+            //for (var simbot of simbots) {
+            //    transformRawData(simbot, 'robot_sim', fields, file)
+            //}
+        });
+    });
 });
 
+
+// file writing process.
+function transformRawData(items, fields, file)
+{
+    // write csv.
+    try {
+        var result = json2csv({ data: items, fields: fields });
+        //console.log(result);
+
+        fs.writeFile("./"+file+".csv", result, function (err) {
+            if (err) {return console.log(err);}
+
+            console.log("The file was saved!");
+        });
+
+    } catch (err) {
+        console.error(err);
+    }
+
+    // write json.
+    fs.writeFile("./" + file, JSON.stringify(items), function (err) {
+        if (err) { return console.log(err); }
+
+        console.log("The file was saved!");
+    });
+
+}
